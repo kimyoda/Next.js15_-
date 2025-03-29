ab11379245078ec12e54622acbdb22e772ab5282
kimyoda <rladygks1210@naver.com>
2025-03-29 20:58:56 +0900

    [feat] section02_2 프리페칭

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -13,6 +14,10 @@ export default function App({ Component, pageProps }: AppProps) {
     // back 페이즈를 뒤로 이동
     router.push("/test");
   };
+  // prefetching 방법 link가 아닌 prefetch를 통해 명시적으로 프리페칭을 설정할 수 있다.
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
 
   return (
     <>
@@ -20,7 +25,10 @@ export default function App({ Component, pageProps }: AppProps) {
         {/* 1. 링크로 이동시키는 방법 */}
         <Link href={"/"}>index</Link>
         &nbsp;
-        <Link href={"/search"}>search</Link>
+        {/* 프리페칭을 강제로 해제하는 방법 , prefetch를 false로 설정*/}
+        <Link href={"/search"} prefetch={false}>
+          search
+        </Link>
         &nbsp;
         <Link href={"/book/1"}>book/1</Link>
         <div>
