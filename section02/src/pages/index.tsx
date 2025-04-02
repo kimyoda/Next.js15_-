950afc0e8d6cd4a6eac7c21f950ccb57f36f4f3a
kimyoda <rladygks1210@naver.com>
2025-04-02 21:57:50 +0900

    [feat] 한입북스 css, ui 세팅완료

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -4,14 +4,28 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
+// @는 tsconfig.json 안에 경로로 있다.
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Home() {
   // 인라인 스타일 지정 가능
   return (
-    <>
-      <h1 className={style.h1}>인덱스</h1>
-      <h2 className={style.h2}>H2</h2>
-    </>
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {/* books의 key값을 받고 전개연산자로 props전달 */}
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map((book) => (
+          <BookItem key={book.id} {...book} />
+        ))}
+      </section>
+    </div>
   );
 }
 
