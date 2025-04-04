410f1591650dd3b130d3786168f445b048b7843b
kimyoda <rladygks1210@naver.com>
2025-04-05 02:45:47 +0900

    [feat] ssr 간단한 실습

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -3,13 +3,37 @@
 
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 // @는 tsconfig.json 안에 경로로 있다.
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
 
-export default function Home() {
-  // 인라인 스타일 지정 가능
+// 인덱스 경로로 접근하면 먼저 실행되는 함수
+export const getServerSideProps = () => {
+  // 컴포넌트보다 먼저 실행되어서, 해당 컴포넌트에 필요한 백엔드 데이터 등을 미리 불러오는 역할을 하는 함수다.
+
+  console.log("서버사이드프롭스에요");
+  // 서버측에서만 실행되는 함수이다.
+  const data = "hello";
+  // 객체를 반드시 반환해야 한다. 객체 타입의 props를 받는다.
+  // Next 앱이 객체를 받아와서 페이지에 전달할 수 있다. 프레임워크의 문법이다.
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data);
+  // 브라우저에 사용 가능한 방법
+  useEffect(() => {
+    console.log(window);
+  }, []);
+
   return (
     <div className={style.container}>
       <section>
