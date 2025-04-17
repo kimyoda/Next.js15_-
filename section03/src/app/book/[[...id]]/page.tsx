eeedff47c12728ac6833df466a112789ce7b3355
kimyoda <rladygks1210@naver.com>
2025-04-18 03:19:19 +0900

    [feat] book page param 수정

--- section03/src/app/book/[[...id]]/page.tsx
+++ section03/src/app/book/[[...id]]/page.tsx
@@ -2,11 +2,6 @@
 // app/book/[[...id]]/page.tsx는 동적 라우팅을 사용하는 페이지입니다.
 // [[...id]]는 선택적 캐치-올 세그먼트로, '/book' 또는 '/book/1' 등 다양한 경로를 처리할 수 있습니다.
 // params를 통해 동적 세그먼트의 값을 받아올 수 있습니다.
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id } = await params;
-  return <div>book/ [id] 페이지 : {id}</div>;
+export default async function Page({ params }: { params: { id: string } }) {
+  return <div>book/ [id] 페이지 : {params.id}</div>;
 }
