7762822a1ac467831a9501f5afeb6071a529e730
kimyoda <rladygks1210@naver.com>
2025-05-04 02:21:01 +0900

    [feat] 인덱스 풀 라우트 캐시 추가

---
+++ section05/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,33 @@
+import BookItem from "@/components/book-item";
+import { BookData } from "@/types";
+
+// 검색 페이지 컴포넌트
+export default async function Page({
+  searchParams, // URL의 쿼리 파라미터를 받는 객체
+}: {
+  searchParams: Promise<{ q?: string }>; // q는 검색어를 나타내는 선택적 파라미터
+}) {
+  // 검색어를 추출
+  const { q } = await searchParams;
+
+  // API 서버에 검색 요청을 보내고 결과를 가져옴
+  // 기본 캐싱: 옵션 미지정 시 정적 생성 캐시 사용
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다!</div>;
+  }
+
+  // 검색 결과를 BookData 타입의 배열로 변환
+  const books: BookData[] = await response.json();
+
+  // 검색 결과를 BookItem 컴포넌트로 렌더링
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
